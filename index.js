180dd1474ef8c05dc364dfb7f1f4290f20780690
AyushCoder9 <ayushsingh.ofc1@gmail.com>
2025-11-25 09:58:35 +0530

    book create route

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const prisma = require("@prisma/client");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const app = express();
+app.use(express.json());
+
+app.post("/books", async (req, res) => {
+  const { title, summary, isbn, url } = req.body;
+  await prisma.book.create({ title, summary, isbn, url });
+  return res.status(201).json("successfully created");
+});
+
+app.get();
